46e9f2331b0da871906d887dee680711d9823280
wofec <wofecchuan@gmail.com>
2020-01-16 23:52:47 +0400

    update

--- files/js/script.js
+++ files/js/script.js
@@ -11,6 +11,8 @@ const gameElements = {
   modalContent: document.querySelector("#modal-content"),
   gameBoard: document.querySelector("#game-board"),
   gameRules: document.querySelector("#rules"),
+  playerName: document.querySelector("#player-name"),
+  playerScore: document.querySelector("#player-score"),
 }
 
 const GAME_STATE_PREPARE = 'GAME_STATE_PREPARE';
@@ -18,6 +20,7 @@ const GAME_STATE_LOGIN = 'GAME_STATE_LOGIN';
 const GAME_STATE_NONE = 'GAME_STATE_NONE';
 const GAME_STATE_CHOOSE_CARD_SHIRT = 'GAME_STATE_CHOOSE_CARD_SHIRT';
 const GAME_STATE_CHOOSE_DIFFICULT = 'GAME_STATE_CHOOSE_DIFFICULT';
+const GAME_STATE_IN_PROGRESS = 'GAME_STATE_IN_PROGRESS';
 
 const cardShirts = ['card-shirt1', 'card-shirt2', 'card-shirt3'];
 const cardsBack = ['cardback1', 'cardback2', 'cardback3'];
@@ -37,6 +40,9 @@ const gameData = {
   cardShirt: null,
   cardsQuantity: null,
   cardBackArr: [],
+  controller: null,
+  gameScore: 0,
+  updateScore: null,
 }
 
 // init block
@@ -52,6 +58,7 @@ function checkPlayer() {
     regNewPlayer();
   }
   showModal();
+  activateUserBar();
 }
 
 function showModal() {
@@ -89,7 +96,7 @@ function actionController({target}) {
     }
 
     case GAME_STATE_IN_PROGRESS: {
-      checkCardValue();
+      gameData.controller(target);
       break;
     }
 
@@ -99,7 +106,7 @@ function actionController({target}) {
 }
 
 function regNewPlayer() {
-  gameElements.loginWrapper.style.display = 'flex';
+  gameElements.loginWrapper.style.display = 'block';
   gameElements.acceptButton.style.display = 'flex';
   gameElements.modalHeader.innerHTML = headerMessages.loginStage;
 }
@@ -113,6 +120,13 @@ function loginUser() {
   localStorage.setItem('mmg_player', name);
   gameData.player.name = name;
   chooseCardShirt();
+}
+
+function activateUserBar() {
+  gameElements.playerName.style.display = 'flex';
+  gameElements.playerName.innerHTML = gameData.player.name;
+  gameElements.playerScore.style.display = 'flex';
+  gameElements.playerScore.innerHTML = 'Score: 0';
 }
 
 function chooseCardShirt() {
@@ -139,20 +153,25 @@ function chooseDifficult({dataset}) {
   gameElements.modalContent.innerHTML = '';
 
   cardsQuantity.forEach((item, index) => {
-    gameElements.modalContent.appendChild(generateCard({quantity: item, id: index}));
+    gameElements.modalContent.appendChild(
+      generateCard({
+        quantity: item, 
+        id: index, 
+        extraClass: 'quantity',
+      }));
   });
 }
 
 function prepareGameStuff({dataset}) { 
-  gameData.gameState = GAME_STATE_CHOOSE_DIFFICULT;
+  hideModal();
+  gameData.gameState = GAME_STATE_IN_PROGRESS;
   gameData.cardsQuantity = cardsQuantity[dataset.id];
   gameElements.gameRules.style.display = 'none';
   gameElements.gameBoard.style.display = 'flex';
-  hideModal();
   generateArrayOfCardsBack();
-  //init gameCtrl
-  // gameData.getInfo = gameController();
   renderContent();
+  gameData.controller = gameController();
+  gameData.updateScore = computeScore();
 }
 
 function renderContent() {
@@ -167,7 +186,7 @@ function renderContent() {
 }
 
 function generateCard(options) {
-  const template = `<div class="card ${options.extraClass}" data-id="${options.id}">${options.quantity}</div>`;
+  const template = `<div class="card ${options.extraClass}" data-id="${options.id}">${options.quantity || ''}</div>`;
   const card = document.createElement('div');
   card.innerHTML = template;
   card.addEventListener('click', actionController);
@@ -180,7 +199,34 @@ function generateUniqId() {
 
 function gameController() {
   const cardsInfo = {};
-  const comparedObjects = {};
+  const comparedObjects = {
+    first: null,
+    second: null,
+    needRefresh: false,
+  };
+
+  function checkCardValue(target) {
+    const {isOpen} = target.dataset;
+
+    if (isOpen === 'true') {
+      return;
+    } else if (comparedObjects.needRefresh) {
+      refreshCards();
+    }
+
+    const {id} = target.dataset;
+    const info = getInfo(id);
+
+    target.classList.add(info);
+    target.dataset.isOpen = true;
+
+    if (!comparedObjects.first) {
+      comparedObjects.first = target;
+    } else {
+      comparedObjects.second = target;
+      compareIt();
+    }
+  }
 
   function getInfo(id) {
     cardsInfo[id] || generateInfo(id);
@@ -188,14 +234,41 @@ function gameController() {
   }
 
   function generateInfo(id) {
-    cardsInfo[id] = gameData.cardBackArr.splice(Math.floor(Math.random() * gameData.cardBackArr.length));
+    cardsInfo[id] = gameData.cardBackArr.splice(
+      Math.floor(
+        Math.random() * gameData.cardBackArr.length), 1)[0];
   }
 
   function compareIt() {
+    const firstCardInfo = getInfo(comparedObjects.first.dataset.id);
+    const secondCardInfo = getInfo(comparedObjects.second.dataset.id);
 
+    if (firstCardInfo === secondCardInfo) {
+      comparedObjects.first.removeEventListener('click', actionController);
+      comparedObjects.second.removeEventListener('click', actionController);
+      comparedObjects.first = null;
+      comparedObjects.second = null;
+      gameData.updateScore(true);
+    } else {
+      comparedObjects.needRefresh = true;
+      gameData.updateScore();
+    }
   }
 
-  return getInfo;
+  function refreshCards() {
+    const firstCardInfo = getInfo(comparedObjects.first.dataset.id);
+    const secondCardInfo = getInfo(comparedObjects.second.dataset.id);
+    comparedObjects.first.classList.remove(firstCardInfo);
+    comparedObjects.first.dataset.isOpen = false;
+    comparedObjects.second.classList.remove(secondCardInfo);
+    comparedObjects.second.dataset.isOpen = false;
+    comparedObjects.needRefresh = false;
+    comparedObjects.first = null;
+    comparedObjects.second = null;
+    comparedObjects.needRefresh = false;
+  }
+
+  return checkCardValue;
 }
 
 function generateArrayOfCardsBack() {
@@ -204,4 +277,61 @@ function generateArrayOfCardsBack() {
     arr.fill(item)
     gameData.cardBackArr = gameData.cardBackArr.concat(arr);
   });
+}
+
+function computeScore() {
+  const score = {
+    defaultScore: 7,
+    defaultLoseScore: 2,
+    defaultMultiple: 1,
+    multiple: 1,
+  }
+
+  function addPoints() {
+    const points = gameData.gameScore + score.defaultScore * score.multiple;
+    gameData.gameScore = points;
+    score.multiple++;
+    updateScore();
+  }
+
+  function removePoints() {
+    let points = gameData.gameScore - score.defaultLoseScore;
+    if (points < 0) {
+      points = 0;
+    }
+    gameData.gameScore = points;
+    score.multiple = score.defaultMultiple;
+    updateScore();
+  }
+
+  function updateScore() {
+    gameElements.playerScore.innerHTML = `Score: ${gameData.gameScore}`;
+  }
+
+  return function(add = false) {
+    if (add) {
+      addPoints();
+    } else {
+      removePoints();
+    }
+  }
+}
+
+function saveRecord() {
+  let records = localStorage.getItem('mmg_records');
+  if (records) {
+    points = JSON.stringify({
+        name: gameData.player.name, 
+        score: gameData.score
+      });
+    records = JSON.parse(records);
+    records.push(points);
+    localStorage.setItem('mmg_records', records)
+  } else {
+    records = JSON.stringify([{
+      name: gameData.player.name, 
+      score: gameData.score
+    }]);
+    localStorage.setItem('mmg_records', records)
+  }
 }
